8ef9f411a534af6fd855a66aa14f2e3d930d9f27
Owen Morales <morales.owen@icloud.com>
2024-11-21 13:50:08 -0700

    header&footer

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,8 @@
+// Mobile Menu Toggle (please be careful with this)
 const mobileMenu = document.getElementById('mobile-menu');
 const navList = document.querySelector('.nav-list');
 
 mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
 });
